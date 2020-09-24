92904144ba5344999c6a3aa76d4fcd044fa5ba89
Carlos H Brandt <c.brandt@jacobs-university.de>
2020-09-24 20:21:42 +0200

    Add DATASETS to pds init (don't like that. TOFIX)

---
+++ gpt/pds/__init__.py
@@ -0,0 +1,13 @@
+
+DATASETS = {
+    'ctx': {
+        'ptype': 'edr',
+        'instr': 'ctx',
+        'host': 'mro'
+    },
+    'hirise': {
+        'ptype': 'rdrv11',
+        'instr': 'hirise',
+        'host': 'mex'
+    }
+}
