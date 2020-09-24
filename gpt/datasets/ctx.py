32db7f92f666a03720e3c2d1028abe551c48b3c6
Carlos H Brandt <c.brandt@jacobs-university.de>
2020-09-24 23:21:56 +0200

    add datasets

---
+++ gpt/datasets/ctx.py
@@ -0,0 +1,13 @@
+DESCRIPTORS = {
+    'ctx': {
+        'product_image': ('Description','PRODUCT DATA FILE WITH LABEL'),
+        'browse_image': ('Description','BROWSE IMAGE'),
+        'browse_thumbnail': ('Description','THUMBNAIL IMAGE')
+    },
+
+DATA = {
+    'format': 'pds'
+    'query': 'ode'
+}
+
+TARGET = 'Mars'
