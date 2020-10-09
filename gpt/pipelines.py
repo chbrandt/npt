bfba2844c15260150b6f0b3c0c1eadd89605c88c
Carlos H Brandt <c.brandt@jacobs-university.de>
2020-10-09 22:28:14 +0200

    Add Processing

--- gpt/pipelines.py
+++ gpt/pipelines.py
@@ -5,7 +5,7 @@ class Search:
     Namespace only
     """
     @staticmethod
-    def query2geojson(bounding_box, dataset_id, geojson_filename):
+    def run(bounding_box, dataset_id, geojson_filename):
         """
         Write GeoJSON with products intersecting 'bounding_box'
 
@@ -24,7 +24,8 @@ class Search:
         gdf = Search.write_geojson(products, filename=output_filename)
         return gdf
 
-    __call__ = query2geojson
+    query2geojson = run
+    __call__ = run
 
     @staticmethod
     def query_footprints(bbox, dataset=None,
@@ -141,3 +142,86 @@ class Download:
             return file_path
         except:
             return None
+
+
+def _change_file_extension(filename, new_ext):
+    fn = '.'.join(filename.split('.')[:-1] + [new_ext])
+    return fn
+def _add_file_subextension(filename, sub_ext):
+    fs = filename.split('.')
+    fs.insert(-1, sub_ext)
+    fn = '.'.join(fs)
+    return fn
+
+class Processing:
+    """
+    Pre-processing (format+calibration+projection)
+
+    Don't forget init-spice!
+    """
+    @staticmethod
+    def run(filename_init, filename_result, projection="sinusoidal", tmpdir=None):
+        # Create a temp dir for the processing
+        import shutil
+        import tempfile
+        if tmpdir:
+            assert os.path.isdir(tmpdir), """Given tmpdir '{}' does not exist""".format(tmpdir)
+            tempfile.tempdir = tmpdir
+
+        try:
+            tmpdir = tempfile.mkdtemp(prefix='neanias_')
+        except:
+            print("Temporary directory ('{}') could not be created.".format(tmpdir))
+            raise err
+        else:
+            print("Processing temporary dir: '{}'".format(tmpdir))
+
+        try:
+            f_in = shutil.copy(filename_init, tmpdir)
+
+            # FORMAT (pds->isis)
+            from gpt.isis import format
+            # -- Init SPICE kernel
+            format.init_spice(f_in)
+            # -- Transfrom PDS (IMG) into ISIS (CUB) file
+            f_cub = _change_file_extension(f_in, 'cub')
+            format.pds2isis(f_in, f_cub)
+
+            # CALIBRATION
+            from gpt.isis import calibration
+            f_cal = _add_file_subextension(f_cub, 'cal')
+            calibration.radiometry(f_cub, f_cal)
+
+            # MAP-PROJECTION
+            from gpt.isis import projection
+            f_map = _add_file_subextension(f_cal, 'map')
+            _flist = [f_cal]
+            proj_file = projection.define_projection(_flist, projection=projection, tmpdir=tmpdir)
+            projection.map_project(f_cal, f_map, proj_file)
+
+            # FORMAT to TIFF
+
+        except Exception as err:
+            print("OOOPS, something went wrong.")
+            raise err
+        finally:
+            print("Cleaning temporary files/directory ({})".format(tmpdir))
+            shutil.rmtree(tmpdir)
+
+
+        #calibration (radiometry)
+        #projection (image->map)
+        # SUBPRODUCTS (for future processing)
+        # ----------------------
+        #format (isis->tiff in 4326
+        try:
+            Processing.proj_planet2earth(filetmp, fileout)
+        except:
+            print("oops")
+        # HIGH_LEVEL PRODUCTS (for visualization)
+        return fileout
+
+    @staticmethod
+    def proj_planet2earth(filein, fileout):
+        from gpt.raster import warp
+        warp(filein, fileout)
