d058f0f3dbf37dfc5306d114615ab1ada5833d46
Carlos H Brandt <c.brandt@jacobs-university.de>
2020-10-09 22:48:15 +0200

    Define projection for ctx

--- gpt/isis/projection.py
+++ gpt/isis/projection.py
@@ -1,9 +1,14 @@
 from ._sh import isissh
 
-def define_projection(list_files, filename_proj, projection='sinusoidal', precision=0):
-    proj = projection
-    fout = filename_proj
-    isissh.mosrange(FROMLIST=list_files, TO=fout, PROJECTION=proj, PRECISION=precision)
+def define_projection(list_files, projection='sinusoidal', precision=0, tmpdir=None):
+    tmpdir = tmpdir or ''
+    _file_list = os.path.join(tmpdir, 'files_to_project.list')
+    with open(_file_list, 'w') as fp:
+        fp.write('\n'.join(list_files))
+    _file_proj = os.path.join(tmpdir, projection+'.map')
+    isissh.mosrange(FROMLIST=_file_list, TO=_file_proj,
+                    PROJECTION=projection, PRECISION=precision)
+    return _file_proj
 
 def map_project(filename_in, filename_out, filename_proj):
     isissh.cam2map(FROM=filename_in, TO=filename_out, MAP=filename_proj, PIXRES='map')
