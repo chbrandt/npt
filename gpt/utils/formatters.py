66f97f6d9fe08fef539b7c8d7caff45368452dd9
Carlos H Brandt <c.brandt@jacobs-university.de>
2020-10-16 23:36:34 +0200

    Fix geojson through geodataframe nasted 'properties' bug

--- gpt/utils/formatters.py
+++ gpt/utils/formatters.py
@@ -5,8 +5,56 @@ from .bbox import string_2_dict as bbox_string_2_dict
 
 from . import log
 
+# === Inspect functions
+# ref: https://fabianlee.org/2019/09/22/python-using-a-custom-decorator-to-inspect-function-arguments/
+#
+import functools
+import inspect
+
+def logintrospect(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        inspect_decorator(func, args, kwargs)
+        result = func(*args, **kwargs)
+        print("-return " + str(result))
+        return result
+    return wrapper
+
+
+def inspect_decorator(func,args,kwargs):
+  funcname = func.__name__
+  print("function {}()".format(funcname))
+
+  # get description of function params expected
+  argspec = inspect.getargspec(func)
+
+  # go through each position based argument
+  counter = 0
+  if argspec.args and type(argspec.args is list):
+    for arg in args:
+      # when you run past the formal positional arguments
+      try:
+        print("*" + str(argspec.args[counter]) + " = " + str(arg))
+        counter+=1
+      except IndexError as e:
+        # then fallback to using the positional varargs name
+        if argspec.varargs:
+          varargsname = argspec.varargs
+          print("*" + varargsname + " = " + str(arg))
+        pass
+
+  # finally show the named varargs
+  if argspec.keywords:
+    kwargsname = argspec.keywords
+    for k,v in kwargs.items():
+      print("**" + kwargsname + " " + k + " = " + str(v))
+
+# ==========
+
+@logintrospect
 def geojson_2_geodataframe(records):
     assert isinstance(records, list), "Expected a list [{}], instead got {}".format(type(records))
+    gpdrecs = []
     for rec in records:
         geom = rec['geometry']
         if isinstance(geom, str):
@@ -22,8 +70,10 @@ def geojson_2_geodataframe(records):
             except Exception as err:
                 log.error(err)
                 raise err
-        rec['geometry'] = geom
-    gdf = gpd.GeoDataFrame(records)
+        gpdrec = rec['properties']
+        gpdrec['geometry'] = geom
+        gpdrecs.append(gpdrec)
+    gdf = gpd.GeoDataFrame(gpdrecs)
     return gdf
 
 
