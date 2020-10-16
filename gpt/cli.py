bc4a8a6e72b361d27b87d22475cdab460bb40cae
Carlos H Brandt <c.brandt@jacobs-university.de>
2020-10-16 13:55:18 +0200

    Fix cli/search docs + bbox format

--- gpt/cli.py
+++ gpt/cli.py
@@ -4,31 +4,47 @@ Define commands and sub-commands line interface
 import click
 from click import argument,option
 
-from .search import bbox as search_bbox
-from .utils.io import json_2_geojson
-from .utils.formaters import bbox_string_2_dict
+from gpt.search import bbox as search_bbox
+from gpt.utils.formatters import json_2_geojson
+from gpt.utils.bbox import string_2_dict as bbox_string_2_dict
 
+class FormatHelp(click.Group):
+    def format_help_text(self, ctx, formatter):
+        if self.help:
+            formatter.write_paragraph()
+            for line in self.help.split('\n'):
+                formatter.write_text(line)
 
-@click.group()
+@click.group(cls=FormatHelp)
 def main():
     pass
 
 
 @main.command()
-@argument('bbox')
+@argument('provider')
 @argument('dataset')
+@argument('bbox')
 @option('--output', default='', help="GeoJSON filename with query results")
-@option('--provider', default='ode', help="Choose interface/provider to query")
+# @option('--provider', default='ode', help="Choose interface/provider to query")
 def search(bbox, dataset, output, provider='ode'):
     """
-    Query ODE for intersecting footprints to bbox.
+    Query 'provider'/'dataset' for 'bbox'-intersecting data products
 
-    BBOX format: '[min,max,west,east]' (positive east [0:360])
+    \b
+    Attributes:
+    - provider: 'ODE'
+        Query ODE for intersecting footprints to bbox.
+    - dataset: <string>
+        Options are: ['mars/mro/ctx/edr', 'mars/mro/hirise/rdrv11'].
+    - bbox: <string>
+        Format: '[min,max,west,east]' (global positive east [0:360])
+        Eg,
+            "[-0.5,0.5,359.5,0.5]"
 
-    Datasets identifiers. Options are 'mars/mro/ctx/edr', 'mars/mro/hirise/rdrv11'.
     """
     dataset_id = dataset
 
+    bbox = bbox.replace('[','').replace(']','')
     bounding_box = bbox_string_2_dict(bbox)
 
     products = search_bbox(bbox=bounding_box,
@@ -37,7 +53,8 @@ def search(bbox, dataset, output, provider='ode'):
     if output:
         json_2_geojson(products, filename=output)
     else:
-        click.echo(products)
+        import json
+        click.echo(json.dumps(products, indent=2))
 
 
 @main.command()
