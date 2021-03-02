a1278e5befefa78e6aa09297296c6a24a3245cd9
Carlos H Brandt <c.brandt@jacobs-university.de>
2021-03-02 15:06:46 +0100

    Add funtion to create COGs

--- npt/utils/raster.py
+++ npt/utils/raster.py
@@ -1,6 +1,8 @@
+import os
 import rasterio
 
 from . import log
+
 
 def warp(fileinname, fileoutname, dst_crs='EPSG:4326'):
     import numpy as np
@@ -31,22 +33,44 @@ def warp(fileinname, fileoutname, dst_crs='EPSG:4326'):
     return
 
 
-def to_tiff(filename_in, filename_out, format_in):
+def to_tiff(filename_in, filename_out, format_in, cog=False):
     """
     For accepted formats (in): https://gdal.org/drivers/raster/index.html
     """
     format_out = 'GTiff'
     format_in = 'ISIS3' if format_in == 'ISIS' else format_in
+    filename_out_tmp = filename_out + '.tmp' if cog else None
     try:
         src = rasterio.open(filename_in, 'r', driver=format_in)
         data = src.read()
         params = src.meta
         params['driver'] = format_out
-        dst = rasterio.open(filename_out, 'w', **params)
+        if cog:
+            dst = rasterio.open(filename_out_tmp, 'w', **params)
+        else:
+            assert filename_out_tmp is None
+            dst = rasterio.open(filename_out, 'w', **params)
         dst.write(data)
         src.close()
         dst.close()
     except Exception as err:
         raise err
         return None
+    if cog:
+        res = tiff2cog(filename_out_tmp, filename_out)
+        # os.remove(filename_out_tmp)
+    else:
+        assert filename_out_tmp is None
     return filename_out
+
+def tiff2cog(filename_in, filename_out):
+    """
+    Transform GeoTIFF in COG
+    """
+    from npt.isis import sh
+    gdal_cmd = sh.wrap('gdal_translate')
+    cog_args = """
+        -co TILED=YES -co COMPRESS=LZW -co BLOCKXSIZE=512 -co BLOCKYSIZE=512
+        -co COPY_SRC_OVERVIEWS=YES --config  GDAL_TIFF_OVR_BLOCKSIZE 512
+    """.split()
+    res = gdal_cmd(filename_in, filename_out, cog_args)
