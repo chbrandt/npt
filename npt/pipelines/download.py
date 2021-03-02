5f5553b101671c22fc4f3856a116015e93b5daff
Carlos H Brandt <c.brandt@jacobs-university.de>
2021-03-02 15:07:41 +0100

    Implement IMG/LBL metadata parse, add to feature/properties

--- npt/pipelines/download.py
+++ npt/pipelines/download.py
@@ -1,4 +1,5 @@
 import os
+import subprocess
 
 from . import log
 
@@ -46,15 +47,91 @@ def run_props(properties, base_path, progressbar=False):
         if label_path:
             properties['label_path'] = label_path
 
+    if 'label_path' in properties:
+        metaDict = createLblDict(properties['label_path'])
+    else:
+        metaDict = createImgDict(properties['image_path'])
+
+    properties.update(metaDict)
+    #TODO: read metadata from downloaded file, make it to feature/properties
     return properties
 
 
 def _download(url, basepath, progressbar=False):
     _file = url.split('/')[-1]
     file_path = os.path.join(basepath, _file)
+    #TODO: check if file_path is here already, if so, skip downloading
     try:
         _out = download_file(url, filename=file_path, progress_on=progressbar)
     except Exception as err:
         log.error(err)
         return None
     return file_path
+
+
+def createLblDict( filename ):
+    """
+    filename is a 'LBL' (PDS LBL) filename
+    """
+    text = open(filename, 'r').read()
+    return _parseLbl(text)
+
+
+def createImgDict( filename ):
+    """
+    filename is a 'IMG' (PDS Image) filename
+    """
+    cmd = "awk '{print $0; if($0 ~ /^END/){if($1 !~ /END_/){exit}}}' %s" % filename
+    p = subprocess.Popen(cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE,
+                            universal_newlines=True, shell=True)
+    values_txt, err = p.communicate()
+    return _parseLbl(values_txt)
+
+
+def _parseLbl(values_txt):
+    lines = values_txt.splitlines()
+    img_header = {}
+    for line in lines:
+        value = line
+        fields = [v.strip() for v in value.split("=")]
+        key_value = fields[0]
+        fill_value = fields[-1].replace('"',"").rstrip()
+        if 'DATA_SET_ID' == key_value:
+            img_header['datasetId']   = fill_value.replace("-","_").replace(".","_")
+            img_header['observationMode'] = img_header['datasetId'].split("_")[4]
+
+        elif 'PRODUCT_ID' == key_value:
+            img_header['idFromProvider'] = fill_value
+
+        elif 'INSTRUMENT_HOST_NAME' == key_value:
+            img_header['instrumentHostName'] = fill_value
+            img_header['spacecraftId'] = "".join([s[0].upper() for s in fill_value.split()])
+
+        elif 'INSTRUMENT_NAME' == key_value:
+            img_header['instrumentName'] = fill_value
+
+        elif 'INSTRUMENT_ID' == key_value:
+            img_header['instrumentId'] = fill_value
+
+        elif 'TARGET_NAME' == key_value:
+            img_header['targetName'] = fill_value
+
+        elif 'MISSION_PHASE_NAME' == key_value:
+            img_header['missionPhaseName'] = fill_value
+
+        elif 'PRODUCT_CREATION_TIME' == key_value:
+            img_header['productCreationTime'] = fill_value
+
+        elif 'START_TIME' == key_value:
+            img_header['startTime'] = fill_value.replace(" ", "")
+
+        elif 'STOP_TIME' == key_value:
+            img_header['stopTime'] = fill_value.replace(" ", "")
+
+
+    img_header['solarDistance'] = ""
+    img_header['solarLongitude'] = ""
+
+    _keys = list(img_header.keys())
+    _keys.sort()
+    return {k:img_header[k] for k in _keys}
