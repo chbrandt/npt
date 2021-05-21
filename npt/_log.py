279674d65e7bfeb22ab9bd7034c3c8885c1d1d08
Carlos H Brandt <c.brandt@jacobs-university.de>
2021-05-21 23:21:10 +0200

    Include module name in lof message format

--- npt/_log.py
+++ npt/_log.py
@@ -1,5 +1,5 @@
 import logging
 # fmt="%(levelname)s:%(funcName)s():%(lineno)i: %(message)s"
-fmt="%(levelname)s:%(funcName)s(): %(message)s"
+fmt="%(levelname)s:%(module)s.%(funcName)s(): %(message)s"
 logging.basicConfig(level=logging.INFO, format=fmt)
 log = logging.getLogger()
