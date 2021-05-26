306d3d226ed7625cb75e5fe6d7ce079c7b985b36
Carlos H Brandt <c.brandt@jacobs-university.de>
2021-05-26 21:29:26 +0400

    Remove adhoc nodata -9999 value set

--- npt/utils/raster.py
+++ npt/utils/raster.py
@@ -39,28 +39,22 @@ def to_tiff(filename_in, filename_out, format_in, cog=False):
     """
     format_out = 'GTiff'
     format_in = 'ISIS3' if format_in == 'ISIS' else format_in
-    filename_out_tmp = filename_out + '.tmp' if cog else None
+
     try:
-        src = rasterio.open(filename_in, 'r', driver=format_in)
-        data = src.read()
-        params = src.meta
-        params['driver'] = format_out
         if cog:
-            dst = rasterio.open(filename_out_tmp, 'w', **params)
+            res = tiff2cog(filename_in, filename_out)
         else:
-            assert filename_out_tmp is None
+            src = rasterio.open(filename_in, 'r', driver=format_in)
+            data = src.read()
+            params = src.meta
+            params['driver'] = format_out
             dst = rasterio.open(filename_out, 'w', **params)
-        dst.write(data)
-        src.close()
-        dst.close()
+            dst.write(data)
+            dst.close()
+            src.close()
     except Exception as err:
         raise err
         return None
-    if cog:
-        res = tiff2cog(filename_out_tmp, filename_out)
-        # os.remove(filename_out_tmp)
-    else:
-        assert filename_out_tmp is None
     return filename_out
 
 def tiff2cog(filename_in, filename_out):
@@ -69,8 +63,12 @@ def tiff2cog(filename_in, filename_out):
     """
     from npt.isis import sh
     gdal_cmd = sh.wrap('gdal_translate')
+    # cog_args = """
+    #     -a_nodata -9999
+    #     -co TILED=YES -co COMPRESS=LZW -co BLOCKXSIZE=512 -co BLOCKYSIZE=512
+    #     -co COPY_SRC_OVERVIEWS=YES --config  GDAL_TIFF_OVR_BLOCKSIZE 512
+    # """.split()
     cog_args = """
-        -a_nodata -9999
         -co TILED=YES -co COMPRESS=LZW -co BLOCKXSIZE=512 -co BLOCKYSIZE=512
         -co COPY_SRC_OVERVIEWS=YES --config  GDAL_TIFF_OVR_BLOCKSIZE 512
     """.split()
