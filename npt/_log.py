d2ce15673054f55543f3ab98ce35a3b06bfb4bea
Carlos H Brandt <c.brandt@jacobs-university.de>
2022-07-05 22:05:46 +0200

    Include library-path in logging messages

--- npt/_log.py
+++ npt/_log.py
@@ -1,7 +1,29 @@
 import logging
-# fmt="%(levelname)s:%(funcName)s():%(lineno)i: %(message)s"
-fmt="%(levelname)s:%(module)s.%(funcName)s(): %(message)s"
-logging.basicConfig(level=logging.INFO, format=fmt)
+# # fmt="%(levelname)s:%(funcName)s():%(lineno)i: %(message)s"
+# fmt="%(levelname)s:%(module)s.%(funcName)s(): %(message)s"
+# logging.basicConfig(level=logging.INFO, format=fmt)
+# log = logging.getLogger('npt')
+# log.setLevel('INFO')
+# log.set_level = log.setLevel
+
+from pathlib import Path
+_lib_path = Path(__file__).parents[1]
+_leng_path = len(_lib_path.as_posix())
+
+def filter(record: logging.LogRecord):
+    record.package = record.pathname[_leng_path+1:]
+    return record
+
+
 log = logging.getLogger('npt')
-log.setLevel('INFO')
+log.setLevel(logging.INFO)
 log.set_level = log.setLevel
+
+handler = logging.StreamHandler()
+
+fmt="%(levelname)s: [%(name)s - %(package)s] %(funcName)s: %(message)s"
+formatter = logging.Formatter(fmt)
+handler.setFormatter(formatter)
+
+handler.addFilter(filter)
+log.addHandler(handler)
